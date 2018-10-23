6651ceefd7d28131595abeff192d99fa7c10da95
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-10-23 14:15:54 -0600

    debugging

--- routes/movies.js
+++ routes/movies.js
@@ -55,7 +55,8 @@ router.post('/', (req, res, next) => {
 
 
 // UPDATE ONE record for this table
-router.put('/:id', (req, res, next) => {
+router.put('/:id', (req, res, next) =>
+console.log('trying to update')
   knex('movies')
   .where('id', req.params.id)
   .then( (movie) => {
